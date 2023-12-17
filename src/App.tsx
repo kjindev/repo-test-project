b0b8258cdb673a4916b61b62f900b8a63fe7db05
kjin <pkjin.eng@gmail.com>
2023-12-17 14:43:58 +0900

    commit test 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return <div>it's test page</div>;
+  return <div>Testing...</div>;
 }
 
 export default App;
